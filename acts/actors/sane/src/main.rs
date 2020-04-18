0147abccd416de1add7085510aeec20b099cd995
sachin maharana <sachin.nicky@gmail.com>
2020-04-19 01:22:00 +0530

    new project

--- acts/actors/sane/src/main.rs
+++ acts/actors/sane/src/main.rs
@@ -1,3 +1,63 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate diesel;
+
+use diesel::prelude::*;
+use diesel::r2d2::{self, ConnectionManager};
+
+use actix_identity::{CookieIdentityPolicy, IdentityService};
+use actix_web::{middleware, web, App, HttpServer};
+
+mod email_service;
+mod errors;
+mod invitation_handler;
+mod models;
+mod register_handler;
+mod schema;
+mod utils;
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+    std::env::set_var("RUST_LOG", "sane=debug,actix_web=info,actix_server=info");
+    env_logger::init();
+
+    let database_url = std::env::var("DATABASE_URL").expect("Required");
+
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool: models::Pool = r2d2::Pool::builder().build(manager).expect("Failed Pool");
+    let domain: String = std::env::var("DOMAIN").unwrap_or_else(|_| "localhost".to_string());
+
+    HttpServer::new(move || {
+        App::new()
+            .data(pool.clone())
+            .wrap(IdentityService::new(
+                CookieIdentityPolicy::new(utils::SECRET_KEY.as_bytes())
+                    .name("auth")
+                    .path("/")
+                    .domain(domain.as_str())
+                    .max_age_time(chrono::Duration::days(1))
+                    .secure(false),
+            ))
+            .wrap(middleware::Logger::default())
+            .data(web::JsonConfig::default().limit(4096))
+            .service(
+                web::scope("/api")
+                    .service(
+                        web::resource("/invitation")
+                            .route(web::post().to(invitation_handler::post_invitation)),
+                    )
+                    .service(
+                        web::resource("register/{invitation_id}")
+                            .route(web::post().to(register_handler::register_user)),
+                    )
+                    .service(
+                        web::resource("/auth")
+                            .route(web::post().to(auth_handler::login))
+                            .route(web::delete().to(auth_handler::logout))
+                            .route(web::get().to(auth_handler::get_me)),
+                    ),
+            )
+    })
+    .bind("127.0.0.1:3000")?
+    .run()
+    .await
 }
