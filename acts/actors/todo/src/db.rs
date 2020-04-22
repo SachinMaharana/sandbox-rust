7d97d5b5847c6b4ca5eb05d3c0949853309673d9
sachin maharana <sachin.nicky@gmail.com>
2020-04-22 15:57:06 +0530

    refactor

---
+++ acts/actors/todo/src/db.rs
@@ -0,0 +1,10 @@
+use diesel::pg::PgConnection;
+use diesel::r2d2::{ConnectionManager, Pool, PoolError, PooledConnection};
+
+pub type PgPool = Pool<ConnectionManager<PgConnection>>;
+type PgPooledConnection = PooledConnection<ConnectionManager<PgConnection>>;
+
+pub fn init_db(database_url: &str) -> Result<PgPool, PoolError> {
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    Pool::builder().build(manager)
+}
